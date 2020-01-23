c754e73272fda3d6aacd76b6d0df7516d3b23f2e
hiroyasu_ <vetryak@smartworld.team>
2020-01-23 10:41:47 +0700

    fix .env

--- console/controllers/TestController.php
+++ console/controllers/TestController.php
@@ -9,7 +9,7 @@ class TestController extends Controller
     public function actionIndex()
     {
         var_dump(111);
-        var_dump(getenv());
+        var_dump(getenv('PASS'));
         die;
     }
 }
