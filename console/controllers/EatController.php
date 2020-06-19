7a1185212364ce57cb53afae086d828d75ee7fc9
hiroyasu_ <vetryak@smartworld.team>
2020-06-19 09:31:30 +0700

    fix message to eat

--- console/controllers/EatController.php
+++ console/controllers/EatController.php
@@ -19,7 +19,8 @@ class EatController extends Controller
 
         $time = '16:20';
         $message = '@all Ребята, не забудьте заказать еду! Заказ будет отправлен в '.$time.', нужно всем успеть до этого времени.
-           [Таблица еды](https://docs.google.com/spreadsheets/d/1FCC-JUso0_t80OZyGKJ7ZQFZ1T90pQkm612-asNnbpM)';
+           [Таблица еды](https://docs.google.com/spreadsheets/d/1FCC-JUso0_t80OZyGKJ7ZQFZ1T90pQkm612-asNnbpM).
+           А так же, если вы вдруг заболели или не придете на следующий день, то пожалуйста, убери свой заказ.';
         RocketChatHelper::sendMessage($this->channel, $message);
         return 0;
     }
